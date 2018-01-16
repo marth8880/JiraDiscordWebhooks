0f5b1c9c09eba3f3cd7755f641c1f6bbacec2dec
marth8880 <marth8880@gmail.com>
2018-01-16 01:00:51 -0800

    Rename project folders; Update assembly information;

--- JiraDiscordWebhookConsole/Properties/AssemblyInfo.cs
+++ JiraDiscordWebhookConsole/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JiraDiscordWebhookServer")]
+[assembly: AssemblyTitle("JiraDiscordWebhookConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JiraDiscordWebhookServer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("JiraDiscordWebhookConsole")]
+[assembly: AssemblyCopyright("Copyright © Aaron Gilbert 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("156f1b6f-7bf9-48fc-a0cd-d55383f8dfdc")]
+[assembly: Guid("08fb5333-1a74-4362-bfde-6d7501dc3fc3")]
 
 // Version information for an assembly consists of the following four values:
 //
